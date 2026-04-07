6dc5d6c7d6176d8da77cfb4ca4b283b40b908c16
ashikmee12 <ashik.mee12@gmail.com>
2026-04-07 22:10:31 +0600

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "8561879348:AAGPrnWaOJ1b2TpYuMt3Y_spe9sblJ3Ut08"
 OWNER_ID = 7406197326
 DB_NAME = "file_store.db"
-MINI_APP_URL = "https://ha-ad.pages.dev"
+MINI_APP_URL = "https://ha-ad2.onrender.com"
